3343a7310845c2aa4cc4ef8829fd5e2d6b2e6d1e
VannetNang <2024024nang@aupp.edu.kh>
2024-05-20 14:52:29 +0700

    Answer_lab1

--- githublab1.py
+++ githublab1.py
@@ -2,3 +2,13 @@
 # Sample run:
 # Enter the number of minutes:  1000000000
 # 1000000000 minutes is approximately 1902 years and 214 days
+
+num_min = (input("Enter the number of minutes: "))
+num_min = int(num_min)
+
+total_day = num_min // (60 * 24)
+total_year = total_day // 365
+remain_day = total_day % 365
+
+print(f"{num_min} minutes is approximately {total_year} years and {remain_day} days.")
+
