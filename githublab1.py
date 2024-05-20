c038e382cf5e7bf2ba9d4124d53d8fdefaa9c678
Ream Rachna <pichreamr@gmail.com>
2024-05-20 15:57:59 +0700

    updated gitlab1

--- githublab1.py
+++ githublab1.py
@@ -2,6 +2,5 @@ minute = int(input("Enter a minute: "))
 total_days = minute/(60*24)
 years = total_days/365
 remaining_days = total_days % 365
-
 print(minute, "minutes", "is approximately", years, "years", "and", remaining_days, "days")
 
