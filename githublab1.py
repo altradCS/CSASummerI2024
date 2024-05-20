9bcbbd018864457e9b94866ebe25ee5ed802a3d1
w3schools-test <2023500Kheav@aupp.edu.kh>
2024-05-20 14:27:46 +0700

    Done

--- githublab1.py
+++ githublab1.py
@@ -2,3 +2,19 @@
 # Sample run:
 # Enter the number of minutes:  1000000000
 # 1000000000 minutes is approximately 1902 years and 214 days
+
+ask_user = int(input("Enter the minutes: "))
+
+
+def min_to_year_day(minute):
+    total_day = minute / (60 * 24)
+    
+    years = total_day // 365
+    
+    remaining_days = total_day % 365
+    
+    return years, remaining_days
+
+years, days= min_to_year_day(ask_user)
+
+print(f"{ask_user} minutes is approximately {years:.0f} years and {days:.0f} days.")    
