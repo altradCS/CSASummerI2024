3c774d5eef2cc56595d3df52705441b067969b39
SopheakpanhaUM <2024070um@aupp.edu.kh>
2024-05-20 14:47:16 +0700

    lab1

--- githublab1.py
+++ githublab1.py
@@ -2,3 +2,9 @@
 # Sample run:
 # Enter the number of minutes:  1000000000
 # 1000000000 minutes is approximately 1902 years and 214 days
+minute = int(input("Enter a number of minutes: "))
+total_day = minute//(60*24)
+total_year = total_day//365
+current_day = total_day%365
+
+print(f"{minute} is approximately {total_year} years and {current_day} days")
