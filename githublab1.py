ecf632733422f78570b600c5ca7dc9a83883f704
Ream Rachna <pichreamr@gmail.com>
2024-05-20 15:31:02 +0700

    coded

--- githublab1.py
+++ githublab1.py
@@ -1,4 +1,7 @@
-# Prompt user to enter the minutes (e.g., 1 billion), and displays the number of years and days for the minutes. 
-# Sample run:
-# Enter the number of minutes:  1000000000
-# 1000000000 minutes is approximately 1902 years and 214 days
+minute = int(input("Enter a minute: "))
+total_days = minute/(60*24)
+years = total_days/365
+remaining_days = total_days % 365
+
+print(minute, "minutes", "is approximately", years, "years", "and", remaining_days, "days")
+
