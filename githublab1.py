fc99aacff50836014e236cbb3e5225c14ef42b98
Samrach Sophal <2024037sophal@aupp.edu.kh>
2024-05-21 14:14:10 +0700

    Update githublab1.py

--- githublab1.py
+++ githublab1.py
@@ -3,11 +3,12 @@
 # Enter the number of minutes:  1000000000
 # 1000000000 minutes is approximately 1902 years and 214 days
 
-current_minute = int(input('Enter number of minutes: '))
+current_minute = eval(input('Enter number of minutes: '))
 
-total_days = current_minute // (24 * 60)
-
-total_years = total_days // 365
-remaining_days = total_days % 365
-
-print(f'{current_minute} minutes is approximately {total_years} years and {remaining_days} days')  
+if current_minute <= 0:
+    print('Invalid input')
+else:
+    total_days = current_minute // (24 * 60)
+    total_years = total_days // 365
+    remaining_days = total_days % 365
+    print(f'{current_minute} minutes is approximately {total_years} years and {remaining_days} days') 
